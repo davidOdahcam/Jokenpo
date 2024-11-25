1c3cc7a1c6ef380bf910afda6d0f9905586d6821
David dos Santos Machado <david.s.machado99@gmail.com>
2024-11-25 17:16:28 -0300

    feat: adding entities

---
+++ Jokenpo/Entities/Choice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jokenpo.Entities
+{
+    internal class Choice
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Dictionary<int, Choice> Weakness { get; set; } = new Dictionary<int, Choice>();
+
+        public void AddWeakness(Choice choice)
+        {
+            Weakness.Add(choice.Id, choice);
+        }
+    }
+}
---
+++ Jokenpo/Entities/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jokenpo.Entities
+{
+    internal class Player
+    {
+        public string Name { get; set; }
+        public Choice Choice { get; set; }
+    }
+}
